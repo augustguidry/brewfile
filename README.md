b7ff31e88237b00a24279ebfae01574eb0b79508
August A. Guidry <august@augustguidry.com>
2023-05-20 09:54:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# August Guidry's .vimrc
+
+## To do list:
+- [ ] Hilight current line
+- [ ] Spellchecking
+- [ ] Use the clipboard for pasting
+- [ ] Get solarized theme
+	- [ ] Write Python script that changes day / night theme based on system setting
---
+++ brewscript.sh
@@ -0,0 +1,55 @@
+# Brew Files
+#This installs Brew.sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Packages
+brew install --cask anki
+brew install --cask appcleaner
+brew install --cask blender
+brew install --cask calibre
+brew install --cask cyberduck
+brew install --cask darktable
+brew install --cask discord
+brew install --cask firefox
+brew install --cask freac
+brew install --cask gemini
+brew install --cask gimp
+brew install --cask github
+brew install --cask gnucash
+brew install --cask godot
+brew install --cask iina
+brew install --cask iterm2
+brew install --cask jellyfin
+brew install --cask libreoffice
+brew install --cask milkytracker
+brew install --cask musicbrainz-picard
+brew install --cask obsidian
+brew install --cask onyx
+brew install --cask pixelorama
+brew install --cask soulseek
+brew install --cask streamlabs-obs
+brew install --cask tastytrade
+brew install --cask tor-browser
+brew install --cask transmission
+brew install --cask virtualbox
+brew install --cask visual-studio-code
+brew install --cask wine-stable
+brew install --cask xld
+brew install cmus
+brew install deno
+brew install fish
+brew install git
+brew install ispell
+brew install neofetch
+brew install neomutt
+brew install neovim
+brew install newsboat
+brew install node
+brew install pass
+brew install powerline-go
+brew install python@3.9
+brew install tmux
+brew install tree
+brew install vim
+brew install xdg-ninja
+brew install z
